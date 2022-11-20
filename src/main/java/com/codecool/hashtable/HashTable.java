2e7f355972e9f1e87e09b421e050d58f8af9a43a
MiMi0001 <willijiem@gmail.com>
2022-11-20 19:43:04 +0100

    Implemented _Lazy buckets_ user story (not fully)

--- src/main/java/com/codecool/hashtable/HashTable.java
+++ src/main/java/com/codecool/hashtable/HashTable.java
@@ -1,6 +1,8 @@
 package com.codecool.hashtable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class HashTable <K, V>{
 
@@ -9,12 +11,15 @@ public class HashTable <K, V>{
 
     private List<List<Entry>> buckets;
 
+    private void initBuckets() {
+        buckets = new ArrayList<List<Entry>>();
+    }
     private int getBucketIndexForKey(K key) {
-        throw new RuntimeException("FIXME");
+        return key.toString().chars().sum() % bucketsSize; // toString then .chars converts to stream, than sums.
     }
 
     private List<Entry> getBucketAtIndex(int position) {
-        throw new RuntimeException("FIXME");
+        return buckets.get(position);
     }
 
     private Entry findEntryInBucket(K key, List<Entry> bucket) {
@@ -26,6 +31,7 @@ public class HashTable <K, V>{
     }
 
     public void put(K key, V value) {
+        if (buckets == null) initBuckets();
         throw new RuntimeException("FIXME");
     }
 
